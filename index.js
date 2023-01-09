15e0ab9e0fd1d78618e5331135430fcad0a42a88
Jafet Leon-Perez <78179738+jleon124@users.noreply.github.com>
2023-01-09 16:48:12 -0500

    Create dynamic health bar hud

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
 
     draw() {
@@ -159,7 +160,8 @@ function animate() {
     }) &&
         player.isAttacking) {
         player.isAttacking = false
-        console.log('e')
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
 
     if (rectangularCollision({
@@ -168,7 +170,8 @@ function animate() {
     }) &&
         enemy.isAttacking) {
         enemy.isAttacking = false
-        console.log('enemy attack success')
+        player.health -= 20
+        document.querySelector('#playerHealth').style.width = player.health + '%'
     }
 }
 
@@ -203,7 +206,7 @@ window.addEventListener('keydown', (event) => {
             enemy.velocity.y = -20
             break
         case 'ArrowDown':
-            enemy.isAttacking = true
+            enemy.attack()
             break
     }
 })
