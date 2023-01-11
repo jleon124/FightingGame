2759b89311d654dc34e84a3ed244a66f809213ba
Jafet Leon-Perez <78179738+jleon124@users.noreply.github.com>
2023-01-11 16:30:13 -0500

    Add attack hitbox animation

--- js/classes.js
+++ js/classes.js
@@ -49,7 +49,16 @@ class Sprite {
 
 class Fighter extends Sprite {
     // wrap arguments for clean and management
-    constructor({ position, velocity, color = 'red', imageSrc, scale = 1, framesMax = 1, offset = { x: 0, y: 0 }, sprites }) {
+    constructor({ 
+        position, 
+        velocity, 
+        color = 'red', 
+        imageSrc, 
+        scale = 1, 
+        framesMax = 1, 
+        offset = { x: 0, y: 0 }, 
+        sprites, 
+        attackBox = { offset: {}, width: undefined, height: undefined } }) {
         super({
             position,
             imageSrc,
@@ -67,9 +76,9 @@ class Fighter extends Sprite {
                 x: this.position.x,
                 y: this.position.y
             },
-            offset, // same as offset: offset,
-            width: 100,
-            height: 50
+            offset : attackBox.offset, // same as offset: offset,
+            width: attackBox.width,
+            height: attackBox.height
         }
         this.color = color
         this.isAttacking
@@ -90,8 +99,12 @@ class Fighter extends Sprite {
         this.draw()
         this.animateFrames()
 
+        // attack boxes
         this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y
+        this.attackBox.position.y = this.position.y + this.attackBox.offset.y
+
+        // draw attack box
+        // c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
 
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
@@ -109,9 +122,6 @@ class Fighter extends Sprite {
     attack() {
         this.switchSprite('attack1')
         this.isAttacking = true
-        setTimeout(() => {
-            this.isAttacking = false
-        }, 100);
     }
 
     switchSprite(sprite) {
